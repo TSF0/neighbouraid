f062f9a250f7eadee21168086dd95407239102f4
TSF0 <gallwin2914@gmail.com>
2026-03-25 16:06:30 +0530

    Create js/firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,20 @@
+const firebaseConfig = {
+
+apiKey: "YOUR_KEY",
+
+authDomain: "YOUR_DOMAIN",
+
+projectId: "YOUR_PROJECT",
+
+storageBucket: "YOUR_BUCKET",
+
+messagingSenderId: "YOUR_ID",
+
+appId: "YOUR_APP_ID"
+
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
