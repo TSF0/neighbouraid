444cd94726b0c6f84e6c7f9b11a46b43026e773d
TSF0 <gallwin2914@gmail.com>
2026-03-25 16:26:07 +0530

    Create js/admin.js

---
+++ js/admin.js
@@ -0,0 +1,42 @@
+const list=document.getElementById("questions");
+
+db.collection("questions")
+
+.orderBy("time","desc")
+
+.onSnapshot(snapshot=>{
+
+let html="";
+
+snapshot.forEach(doc=>{
+
+let data=doc.data();
+
+html+=`
+
+<div class="question">
+
+<p>${data.text}</p>
+
+<button onclick="deleteQuestion('${doc.id}')">
+
+Delete
+
+</button>
+
+</div>
+
+`;
+
+});
+
+list.innerHTML=html;
+
+});
+
+
+function deleteQuestion(id){
+
+db.collection("questions").doc(id).delete();
+
+}
