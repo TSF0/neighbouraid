e46197d892ad93539259a04822063dca3c2b2da3
TSF0 <gallwin2914@gmail.com>
2026-03-25 16:07:28 +0530

    Create js/auth.js

---
+++ js/auth.js
@@ -0,0 +1,35 @@
+function register(){
+
+let email=document.getElementById("email").value;
+let pass=document.getElementById("password").value;
+
+auth.createUserWithEmailAndPassword(email,pass)
+
+.then(()=>{
+
+alert("Account created");
+
+window.location="dashboard.html";
+
+})
+
+.catch(e=>alert(e.message));
+
+}
+
+function login(){
+
+let email=document.getElementById("email").value;
+let pass=document.getElementById("password").value;
+
+auth.signInWithEmailAndPassword(email,pass)
+
+.then(()=>{
+
+window.location="dashboard.html";
+
+})
+
+.catch(e=>alert(e.message));
+
+}
